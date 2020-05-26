1a0975bbb0e0bac2e527f98bf4bebea1bb388b97
sn49 <gin7174@naver.com>
2020-05-26 14:15:47 +0900

    V1.1.2.2 럭키팡 밸런스 패치     모드에 따라 얻는 비율 조정         1-1.2배 2-1.6배 4-3배 5-4배
    노말랜덤에 어비스 운명의 갈림길 추가
    하드랜덤에 공동묘시 해골성 대탐험 추가

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ if not os.path.isfile(path2):
     tf.close()
 
 
-version="V1.1.2.1"
+version="V1.1.2.2"
 
 print(members)
 
@@ -43,11 +43,11 @@ token = "NjYxMTc5OTgzNzAzNTcyNDkx.Xp5u9Q.ciODDc8YvlAfXS8CjW4ni6lyaHQ"
 
 map1v1 = '비치 해변 드라이브,쥐라기 공룡 무덤,브로디 비밀의 연구소,네모 산타의 비밀공간,빌리지 고가의 질주,월드 리오 다운힐,도검 구름의 협곡,신화 신들의 세계,WKC 코리아 서킷,차이나 서안 병마용'#10
 
-mapnormal = '브로디 비밀의 연구소,월드 뉴욕 대질주,쥐라기 공룡섬 결투장,월드 두바이 다운타운,사막 놀라운 공룡 유적지,신화 신들의 세계,비치 해변 드라이브,빌리지 고가의 질주,WKC 싱가폴 마리나 서킷,WKC 상해 서킷,월드 리오 다운힐,빌리지 익스트림 경기장,빌리지 남산'#13
+mapnormal = '브로디 비밀의 연구소,월드 뉴욕 대질주,쥐라기 공룡섬 결투장,월드 두바이 다운타운,사막 놀라운 공룡 유적지,신화 신들의 세계,비치 해변 드라이브,빌리지 고가의 질주,WKC 싱가폴 마리나 서킷,WKC 상해 서킷,월드 리오 다운힐,빌리지 익스트림 경기장,빌리지 남산,어비스 운명의 갈림길'
 
-maphard='월드 이탈리아 피사의 사탑,WKC 브라질 서킷,네모 산타의 비밀공간,네모 강철바위 용광로,도검 구름의 협곡,대저택 은밀한 지하실,차이나 골목길 대질주,차이나 서안 병마용,황금문명 오리엔트 황금 좌표,황금문명 비밀장치의 위협,해적 로비 절벽의 전투,빌리지 만리장성,어비스 바다 소용돌이,사막 빙글빙글 공사장'#14
+maphard='월드 이탈리아 피사의 사탑,WKC 브라질 서킷,네모 산타의 비밀공간,네모 강철바위 용광로,도검 구름의 협곡,대저택 은밀한 지하실,차이나 골목길 대질주,차이나 서안 병마용,황금문명 오리엔트 황금 좌표,황금문명 비밀장치의 위협,해적 로비 절벽의 전투,빌리지 만리장성,어비스 바다 소용돌이,사막 빙글빙글 공사장,공동묘지 해골성 대탐험'
 
-mapveryhard='노르테유 익스프레스,광산 3개의 지름길,광산 위험한 제련소,광산 꼬불꼬불 다운힐,동화 이상한 나라의 문,쥐라기 공룡섬 대모험,어비스 스카이라인,어비스 숨겨진 바닷길,문힐시티 숨겨진 지하터널,공동묘지 마왕의 초대,포레스트 지그재그,팩토리 미완성 5구역,빌리지 붐힐터널'#13
+mapveryhard='노르테유 익스프레스,광산 3개의 지름길,광산 위험한 제련소,광산 꼬불꼬불 다운힐,동화 이상한 나라의 문,쥐라기 공룡섬 대모험,어비스 스카이라인,어비스 숨겨진 바닷길,문힐시티 숨겨진 지하터널,공동묘지 마왕의 초대,포레스트 지그재그,팩토리 미완성 5구역,빌리지 붐힐터널'
 
 
 mapall=[]
@@ -90,9 +90,9 @@ for i in mapveryhard.split(',') :
 print(mapall)
 
 
-command="도와줘,안녕,에결,에결리스트,노멀,노멀리스트,하드,하드리스트,베리하드,베리하드리스트,전체,전체리스트,버전,추천,가입테스트,가입,모아,베팅,기부,상점,구걸,그리고 숨겨진 몇개의 명령어들"
+command="도와줘,안녕,에결,에결리스트,노멀,노멀리스트,하드,하드리스트,베리하드,베리하드리스트,전체,전체리스트,버전,추천,가입테스트,가입,모아,베팅,기부,상점,구걸,문의,럭키팡,그리고 숨겨진 몇개의 명령어들"
 
-commandlist="도와줘,안녕,에결,에결리스트,노멀,노멀리스트,하드,하드리스트,베리하드,베리하드리스트,전체,전체리스트,버전,추천,킹오hi,욕해줘,아잉련아,새벽,가입테스트,베팅,가입,모아,기부,상점,구걸"
+commandlist="도와줘,안녕,에결,에결리스트,노멀,노멀리스트,하드,하드리스트,베리하드,베리하드리스트,전체,전체리스트,버전,추천,킹오hi,욕해줘,아잉련아,새벽,가입테스트,베팅,가입,모아,기부,상점,구걸,문의,럭키팡"
 
 @bot.event
 async def on_message(ctx) :
@@ -302,19 +302,19 @@ async def 베팅(ctx,moa=None,mode=None,repeat=None) :
                     print("lose : "+str(lose))
                     if int(mode)==1 : 
                         chance=80
-                        multiple=1.4
+                        multiple=1.2
                     if int(mode)==2 : 
                         chance=64
-                        multiple=1.8
+                        multiple=1.6
                     if int(mode)==3 : 
                         chance=48
                         multiple=2.2
                     if int(mode)==4 : 
                         chance=32
-                        multiple=2.6
+                        multiple=3
                     if int(mode)==5 : 
                         chance=16
-                        multiple=3       
+                        multiple=4       
                 if int(mode)<6 and int(mode)>0 : 
                     result=random.randrange(0,100)
                     print(str(result)+" "+str(chance))
@@ -360,7 +360,7 @@ async def 베팅(ctx,moa=None,mode=None,repeat=None) :
                         stat.close()
                         luckym=(math.floor(int(stats[1])*0.1))
                         print(luckym)
-                        for i in range(10) :
+                        for i in range(30) :
                             getusers.append(random.randrange(0,len(members)))
                         await ctx.send(getusers)
                         getuser=random.choice(getusers)
